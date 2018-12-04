4debf400347438e8e975e41f120774768d2a04ba
FriedrichWilhelmNietzsche <2657070472@qq.com>
2018-12-04 23:00:51 +0800

    Update MainWindow.xaml.cs

--- Wpf_20181130/MainWindow.xaml.cs
+++ Wpf_20181130/MainWindow.xaml.cs
@@ -14,23 +14,25 @@ using System.Windows.Shapes;
 
 namespace Wpf_serialport20181202
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
+ public partial class MainWindow : Window
     {
         SerialPort mySerialPort = new SerialPort();          // 实例化一个串口
+        private StringBuilder builder = new StringBuilder();  // 字符串构造器
+        private long received_count = 0;//接收计数   
+        private long send_count = 0;//发送计数   
+
+
         public MainWindow()
         {         
 
             InitializeComponent();
 
-            Portsets();             // 设置串口
+            PortSets();             // 设置串口
 
 
         }
 
-        private void Portsets()
+        private void PortSets()
         {            
             foreach (string str in SerialPort.GetPortNames())    // 遍历串口
             {
@@ -39,67 +41,64 @@ namespace Wpf_serialport20181202
            
             string[] BaudRateArr = new string[] { "9600", "115200" };          // 波特率集合
             BaudRate.ItemsSource = BaudRateArr;                          
-            BaudRate.SelectedIndex = 0;                                                
-            mySerialPort.Parity = Parity.None;                  // 奇偶校验检查协议
-            mySerialPort.StopBits = StopBits.One;          // 每个字节的标准停止位数
-            mySerialPort.DataBits = 8;                             // 每个字节的标准数据位长度
-            //mySerialPort.Handshake = Handshake.None;            
+            BaudRate.SelectedIndex = 1;
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);    // 添加数据到达事件
             mySerialPort.NewLine = "\r\n";
-            mySerialPort.RtsEnable = true;
-            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);    // 添加数据到达事件
         }
-
-        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            SerialPort sp = (SerialPort)sender;
-            string indata = sp.ReadExisting();   //以字符串形式，读取串口的所有数据(编码方式取决于	Encoding属性的值)     
-            //Read(Byte[], Int32, Int32) --  从 串口输入缓冲区读取一些字节，并将那些字节	写入字节数组中指定的偏移量处
-
-            DataReceived.Content = indata;     // 显示
-        }
-
             private void OpenButton_Click(object sender, RoutedEventArgs e)                      // 打开串口
-        {
-            if (mySerialPort.IsOpen)
             {
-                MessageBox.Show("串口不能重复打开！");
-            }
-            else
-            {
-                try
-                {
-                    mySerialPort.PortName = myPortName.Text;             // 串口
-                    mySerialPort.BaudRate = int.Parse(BaudRate.Text);   // 波特率
-                    mySerialPort.Open();
-                }
+               try
+               {
+                  mySerialPort.PortName = myPortName.Text;             // 串口名
+                  mySerialPort.BaudRate = int.Parse(BaudRate.Text);   // 波特率,字符串型数值转换为整数
+                  mySerialPort.Open();
+                  OpenButton.IsEnabled = false;   // 重复按无效
+                  CloseButton.IsEnabled = true;    
+                  SendButton.IsEnabled = true;
+               }
                 catch (Exception err)
-                {
-                    mySerialPort = new SerialPort();
-                    MessageBox.Show(err.Message);
-                }
-            }
-           
-        }
-
+               {               
+                  MessageBox.Show(err.Message);
+               }
+             }         
+      
         private void CloseButton_Click(object sender, RoutedEventArgs e)    // 关闭串口
         {
-            if (mySerialPort.IsOpen)
+            try
             {
                 mySerialPort.Close();
+                OpenButton.IsEnabled = true;   // 打开串口按键可用
+                CloseButton.IsEnabled = false;
+                SendButton.IsEnabled = false;
             }
-            else
+            catch 
             {
-                MessageBox.Show("串口已关闭");
+               // MessageBox.Show(err.Message);
             }
         }
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            int m = 0,  n = 0; // m: 温度发送至25，n: 湿度发送65
-
-            List<byte> WriteBuffer = new List<byte>();  //实例化一个byte列表用于存储
+            byte[] Data = new byte[1];     //一次发送一个字节
+            for (int i = 1; i < (SendTextBox.Text.Length - SendTextBox.Text.Length % 2) / 2; i++)      //输入两个等于一个字节
+            {
+                Data[0] = Convert.ToByte(SendTextBox.Text.Substring(i * 2, 2), 16);            //对输入框中的内容依次取2个，并转换为16进制byte型数值
+                mySerialPort.Write(Data, 0, 1);               //对Data数组中的内容从偏移量为0的位置写入指定1个字节长度的内容
+            }
+            if (SendTextBox.Text.Length % 2 != 0)          //若输入内容长度不为偶数
+            {
+                MessageBox.Show("Error");
+            }
+        }
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int n = mySerialPort.BytesToRead;     //先记录下来，避免缓存不一致   
+            byte[] buf = new byte[n];                  //声明一个临时数组存储当前来的串口数据   
+            received_count += n;                    //增加接收计数   
+            mySerialPort.Read(buf, 0, n);       //读取缓冲数据   
 
 
         }
     }
 }
+
